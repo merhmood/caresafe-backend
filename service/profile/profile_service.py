f58902b56db5127233d28d798019840a0e9cd5b2
merhmood <mahmudsuberu9@gmail.com>
2024-05-21 06:08:42 +0100

    refactor(profile): formatted profile_service

--- service/profile/profile_service.py
+++ service/profile/profile_service.py
@@ -6,15 +6,13 @@ class ProfileService():
     @staticmethod
     def get_profile_details(user_id):
         profile = Profile.query.filter_by(user_id=user_id).first()
-        if profile is not None:
-            return {
-                'imgUrl': profile.img_url, 
-                'name': profile.name, 
-                'address': profile.address, 
-                'remoteAppointmentsThreshold': profile.remote_appointments_threshold, 
-                'dailyAppointmentsThreshold': profile.daily_appointments_threshold
-            }
-        return {}
+        return {
+            'imgUrl': profile.img_url, 
+            'name': profile.name, 
+            'address': profile.address, 
+            'remoteAppointmentsThreshold': profile.remote_appointments_threshold, 
+            'dailyAppointmentsThreshold': profile.daily_appointments_threshold
+        }
     
     def create_profile(user_id, name, address):
         new_profile = Profile(
@@ -29,19 +27,10 @@ class ProfileService():
 
     def update_profile_details(user_id, new_profile_details):
         profile = Profile.query.filter_by(user_id=user_id).first()
-        if profile is not None:
-            profile.name = new_profile_details['name']
-            profile.address = new_profile_details['address']
-            profile.daily_appointments_threshold = new_profile_details['dailyAppointmentsThreshold']
-            profile.remote_appointments_threshold = new_profile_details['remoteAppointmentsThreshold']
-        else:
-            new_profile = Profile(
-                user_id=user_id, 
-                name=new_profile_details['name'], 
-                address=new_profile_details['address'], 
-                daily_appointments_threshold=100, 
-                remote_appointments_threshold=20
-            )
-            db.session.add(new_profile)
+        profile.name = new_profile_details['name']
+        profile.address = new_profile_details['address']
+        profile.daily_appointments_threshold = new_profile_details['dailyAppointmentsThreshold']
+        profile.remote_appointments_threshold = new_profile_details['remoteAppointmentsThreshold']
+
         db.session.commit()
         return {'message': 'profile updated'}
